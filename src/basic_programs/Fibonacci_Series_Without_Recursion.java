ac54de9b733be541288bbb2dc0eb1d1eceabe1c9
anuragsadula <sadulaanurag@gmail.com>
2022-03-20 09:47:18 +0530

    Initial commit for Fibonacci_Series_Without_Recursion

---
+++ src/basic_programs/Fibonacci_Series_Without_Recursion.java
@@ -0,0 +1,24 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series_Without_Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int a = 0, b = 1, c;
+			Scanner input = new Scanner(System.in);
+			System.out.print("How many Fibonacci numbers do you want to print: ");
+			int n = input.nextInt();
+			System.out.print(a+" "+b+" ");
+			for(int i=1;i<n;i++) {
+				
+				c = a + b;
+				System.out.print(c+" ");
+				a = b;
+				b = c;
+			}
+	}
+
+}
